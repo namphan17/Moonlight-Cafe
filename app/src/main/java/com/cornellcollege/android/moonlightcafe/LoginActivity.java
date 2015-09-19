ae1ed4c3b6b2b0845e9da457f4626252af017c04
Oldkin <surti.akash@gmail.com>
2015-09-19 16:51:41 -0500

    Minor changes

--- app/src/main/java/com/cornellcollege/android/moonlightcafe/LoginActivity.java
+++ app/src/main/java/com/cornellcollege/android/moonlightcafe/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.cornellcollege.android.moonlightcafe;
 
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 
@@ -7,16 +8,18 @@ import android.widget.EditText;
  * Created by akashsurti on 9/19/15.
  */
 
-public class LoginActivity {
+public class LoginActivity extends AppCompatActivity{
+
     EditText username = (EditText) findViewById(R.id.login_id);
     EditText password = (EditText) findViewById(R.id.login_password);
-    public void login(View view){
+    public void login(View view) {
 
         // Correct password
-        if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+        if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
 
         // Wrong password
-        } else{
+        } else {
 
         }
+    }
 }
