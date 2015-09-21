c0d50425f3433b0f590cc1d76aa939d0451faa01
dtsigie <dawit9330@yahoo.com>
2015-09-21 15:13:26 -0500

    Added Backend

--- app/src/main/java/com/cornellcollege/android/moonlightcafe/LoginActivity.java
+++ app/src/main/java/com/cornellcollege/android/moonlightcafe/LoginActivity.java
@@ -3,7 +3,7 @@ package com.cornellcollege.android.moonlightcafe;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
